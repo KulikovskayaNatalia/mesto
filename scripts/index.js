2f7ec3d0b046a013d6ca5feb265d44df1932e47d
Natalia Kulikovskaya <ns.kulikovskaya@gmail.com>
2022-05-30 00:08:07 +0300

    style: добавить popup

---
+++ scripts/index.js
@@ -0,0 +1,5 @@
+const editButton = document.querySelector('.profile__edit-button');
+const popup = document.querySelector(selectors: '.popup');
+editButton.addEventListener('click', listner: function()) {
+    popup.classList.add('popup__is-open');
+}
